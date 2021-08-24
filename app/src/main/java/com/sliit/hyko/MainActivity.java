8607d6353dc6f440e5d67229d3e01d2b3e560992
Flashminat0 <wyasaslowe@gmail.com>
2021-08-24 21:50:27 +0530

    navigation bar works
    bare bones
    style it up a little bit

--- app/src/main/java/com/sliit/hyko/MainActivity.java
+++ app/src/main/java/com/sliit/hyko/MainActivity.java
@@ -1,14 +1,100 @@
 package com.sliit.hyko;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.sliit.hyko.Fragment.HomeFragment;
+import com.sliit.hyko.Fragment.NotificationFragment;
+import com.sliit.hyko.Fragment.ProfileFragment;
+import com.sliit.hyko.Fragment.SearchFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    Fragment selectedFragment = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListner);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container , new HomeFragment()).commit();
+
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListner =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_search:
+                            selectedFragment = new SearchFragment();
+                            break;
+                        case R.id.nav_add:
+                            selectedFragment = null;
+                            startActivity(new Intent(MainActivity.this , PostActivity.class));
+                            break;
+                        case R.id.nav_heart:
+                                        selectedFragment = new NotificationFragment();
+                            break;
+                        case R.id.nav_profile:
+                            SharedPreferences.Editor editor = getSharedPreferences("PREFS", MODE_PRIVATE ).edit();
+                            editor.putString("profiled", FirebaseAuth.getInstance().getCurrentUser().getUid());
+                            editor.apply();
+                            selectedFragment = new ProfileFragment();
+                            break;
+                    }
+                           if (selectedFragment != null){
+                               getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container , selectedFragment).commit();
+                           }
+
+                    return false;
+                }
+            };
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
