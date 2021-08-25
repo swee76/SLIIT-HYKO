085290d24afa78db952f928fc349455d8d41dac0
Kalana Wickramaarachchi <64887137+lonewol7f@users.noreply.github.com>
2021-08-25 20:56:33 +0530

    design of comment activity

---
+++ app/src/main/java/com/sliit/hyko/CommentActivity.java
@@ -0,0 +1,14 @@
+package com.sliit.hyko;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CommentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comment);
+    }
+}
