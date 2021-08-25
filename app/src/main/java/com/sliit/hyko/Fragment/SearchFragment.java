1518286eec64318a6101e0e6d7147d46825b72b5
Flashminat0 <wyasaslowe@gmail.com>
2021-08-25 12:31:08 +0530

    search can now search users

--- app/src/main/java/com/sliit/hyko/Fragment/SearchFragment.java
+++ app/src/main/java/com/sliit/hyko/Fragment/SearchFragment.java
@@ -1,7 +1,9 @@
 package com.sliit.hyko.Fragment;
 
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +17,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.hendraanggrian.appcompat.widget.SocialAutoCompleteTextView;
 import com.sliit.hyko.Adapter.UserAdapter;
@@ -49,6 +52,24 @@ public class SearchFragment extends Fragment {
 
 
         readUsers();
+
+        search_bar.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                searchUser(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+//                filter(s.toString());
+            }
+        });
+
         return view;
     }
 
@@ -74,4 +95,28 @@ public class SearchFragment extends Fragment {
             }
         });
     }
+
+    private void searchUser (String s) {
+        Query query = FirebaseDatabase.getInstance().getReference().child("Users")
+                .orderByChild("username").startAt(s).endAt(s + "\uf8ff");
+
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                mUsers.clear();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    User user = snapshot.getValue(User.class);
+                    mUsers.add(user);
+                }
+                userAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+
 }
