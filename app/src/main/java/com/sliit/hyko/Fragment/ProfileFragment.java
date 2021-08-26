22e5e8bf7332e1ac8a8c2bf8c2b1a8c9bb88352e
Osini Kithma <66475844+swee76@users.noreply.github.com>
2021-08-26 11:40:45 +0530

    Now user can see his own posts in the ProfileFragment

--- app/src/main/java/com/sliit/hyko/Fragment/ProfileFragment.java
+++ app/src/main/java/com/sliit/hyko/Fragment/ProfileFragment.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -20,11 +21,14 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.sliit.hyko.Adapter.PhotoAdapter;
 import com.sliit.hyko.Model.Post;
 import com.sliit.hyko.Model.User;
 import com.sliit.hyko.R;
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -33,11 +37,11 @@ import de.hdodenhof.circleimageview.CircleImageView;
 public class ProfileFragment extends Fragment {
 
     private RecyclerView recyclerViewSaves;
-//    private PhotoAdapter postAdapterSaves;
+   private PhotoAdapter postAdapterSaves;
     private List<Post> mySavedPosts;
 
     private RecyclerView recyclerView;
-//    private PhotoAdapter photoAdapter;
+    private PhotoAdapter photoAdapter;
     private List<Post> myPhotoList;
 
     private CircleImageView imageProfile;
@@ -91,9 +95,18 @@ public class ProfileFragment extends Fragment {
         savedPictures = view.findViewById(R.id.saved_pictures);
         editProfile = view.findViewById(R.id.edit_profile);
 
+        recyclerViewSaves = view.findViewById(R.id.recucler_view_saved);
+        recyclerViewSaves.setHasFixedSize(true);
+        recyclerViewSaves.setLayoutManager(new GridLayoutManager(getContext(), 3));
+        mySavedPosts = new ArrayList<>();
+        postAdapterSaves = new PhotoAdapter(getContext(), mySavedPosts);
+        recyclerViewSaves.setAdapter(postAdapterSaves);
+
+
         userInfo();
         getFollowersAndFollowingCount();
         getPostCount();
+        myPhotos();
 
         if (profileId.equals(fUser.getUid())) {
             editProfile.setText("Edit profile");
@@ -127,6 +140,30 @@ public class ProfileFragment extends Fragment {
         });
 
         return  view;
+    }
+
+    private void myPhotos() {
+        FirebaseDatabase.getInstance().getReference().child("Posts").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                myPhotoList.clear();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Post post = snapshot.getValue(Post.class);
+
+                    if (post.getPublisher().equals(profileId)) {
+                        myPhotoList.add(post);
+                    }
+                }
+
+                Collections.reverse(myPhotoList);
+                photoAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     private void checkFollowingStatus() {
